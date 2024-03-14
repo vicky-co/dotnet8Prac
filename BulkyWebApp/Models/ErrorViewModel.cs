7ce772bb36c1bed45a7c0a6ffd8c372bc2b40ec6
Paresh Kumar Sethy <pareshsethy1@gmail.com>
2024-03-14 15:14:21 +0530

    Add project files.

---
+++ BulkyWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BulkyWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
