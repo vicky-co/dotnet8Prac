eddacf3642f45b6734bab65ff96eacbcce390687
paresh sethy <pareshsethy1@gmail.com>
2024-03-20 23:54:00 +0530

    added dbcontext and category controller

---
+++ BulkyWebApp/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using BulkyWebApp.Data;
+using BulkyWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWebApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        ApplicationDbContext db;
+        public CategoryController(ApplicationDbContext _db)
+        {
+            db = _db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> categories = db.Categories.ToList();
+            return View(categories);
+        }
+    }
+}
