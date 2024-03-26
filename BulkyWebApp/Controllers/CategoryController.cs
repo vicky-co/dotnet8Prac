f367d8875ff07c9acafc78d8414a4fa9d67d320d
paresh sethy <pareshsethy1@gmail.com>
2024-03-26 09:54:17 +0530

    category crud

--- BulkyWebApp/Controllers/CategoryController.cs
+++ BulkyWebApp/Controllers/CategoryController.cs
@@ -16,5 +16,87 @@ namespace BulkyWebApp.Controllers
             List<Category> categories = db.Categories.ToList();
             return View(categories);
         }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if (category.Name == "test")
+            {
+                ModelState.AddModelError("Name", "Category Name Cannot be test");
+            }
+            Category? existCategory = db.Categories.FirstOrDefault(c => c.Name == category.Name && c.DisplayOrder == category.DisplayOrder);
+            if (existCategory!= null)
+            {
+                ModelState.AddModelError("", "Entry Already Exist");
+            }
+            if (ModelState.IsValid)
+            {
+                db.Categories.Add(category);
+                db.SaveChanges();
+                TempData["Success"] = "Category Created Successfully.";
+                return RedirectToAction("Index");
+            } 
+            return View();
+        }        
+        public IActionResult Edit(int? catId)
+        {
+            if(catId == null)
+            {
+                return NotFound();
+            }
+            Category? category = db.Categories.FirstOrDefault(c => c.Id == catId);
+            return View(category);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if(category.Name == "test")
+            {
+                ModelState.AddModelError("Name","Category Name Cannot be test");
+            }
+            Category? existCategory = db.Categories.FirstOrDefault(c => c.Name == category.Name && c.DisplayOrder == category.DisplayOrder);
+            if (existCategory != null)
+            {
+                ModelState.AddModelError("", "Entry Already Exist");
+            }
+            if (ModelState.IsValid)
+            {
+                db.Categories.Update(category);
+                db.SaveChanges();
+                TempData["Success"] = "Category Updated Successfully.";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Category? category = db.Categories.FirstOrDefault(c => c.Id == id);
+            return View(category);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? category = db.Categories.FirstOrDefault(c => c.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            db.Categories.Remove(category);
+            db.SaveChanges();
+            TempData["Success"] = "Category Deleted Successfully.";
+            return RedirectToAction("Index");
+            /*return View("Index","Category");*/
+        }
     }
 }
--- BulkyWebApp/Models/Category.cs
+++ BulkyWebApp/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkyWebApp.Models
 {
@@ -7,7 +8,11 @@ namespace BulkyWebApp.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [DisplayName("Category Name")]
+        [StringLength(50 ,ErrorMessage ="String length should be between 1 to 50")]
         public string Name { get; set; }
+        [Range(0, 150, ErrorMessage ="Order should be between the range 1 to 150")]
+        [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
     }
 }
